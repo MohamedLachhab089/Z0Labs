11a2d800ae881c2ec2e4b9e489ed4ad25fe96587
Mohamed Lachhab <mohamed15lachhab@gmail.com>
2026-03-05 17:57:33 +0000

    feat(customer): add POST endpoint to create customer

--- Customer/src/main/java/org/med/customer/controller/CustomerController.java
+++ Customer/src/main/java/org/med/customer/controller/CustomerController.java
@@ -1,10 +1,28 @@
 package org.med.customer.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.med.customer.dto.CustomerRequest;
+import org.med.customer.service.CustomerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/customers")
 @RequiredArgsConstructor
-public class CustomerController {}
+public class CustomerController {
+
+  private final CustomerService customerService;
+
+  @PostMapping
+  public ResponseEntity<String> createCustomer(
+      @RequestBody @Valid CustomerRequest customerRequest) {
+    return ResponseEntity.ok(customerService.createCustomer(customerRequest));
+  }
+
+  @PutMapping
+  public ResponseEntity<Void> updateCustomer(@RequestBody @Valid CustomerRequest customerRequest) {
+    customerService.updateCustomer(customerRequest);
+    return ResponseEntity.accepted().build();
+  }
+}
